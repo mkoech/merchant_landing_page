368efd52439b06e93e15ce96d21680b040b95d00
mkoech <48014492+mkoech@users.noreply.github.com>
2021-05-04 09:56:11 +0300

    Update main.dart
    Edit on expansion tile

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+imimport 'package:flutter/material.dart';
 
 void main() => runApp(MerchantsApp());
 
@@ -47,6 +47,9 @@ class HomePage extends StatelessWidget {
 }
 
 class AddCategory extends StatelessWidget {
+  final VoidCallback onDelete;
+
+  AddCategory({Key key, this.onDelete}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -88,34 +91,27 @@ class AddCategory extends StatelessWidget {
               const Divider(
                 height: 20,
               ),
-              Container(
-                color: Colors.blueGrey,
-                child: Padding(
-                  padding: const EdgeInsets.all(6.0),
+              Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Container(
+                  color: Colors.blue,
                   child: ExpansionTile(
-                    title: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Subcategory',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                        ),
+                    backgroundColor: Colors.blue,
+                    title: Text(
+                      'Subcategory',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
                       ),
                     ),
-                    leading: Column(
-                      children: [
-                        Icon(
-                          Icons.keyboard_arrow_down,
-                        ),
-                        Icon(
-                          Icons.keyboard_arrow_up,
-                        ),
-                      ],
+                    leading: IconButton(
+                      icon: Icon(Icons.keyboard_arrow_down),
+                      onPressed: () {},
                     ),
-                    trailing: Icon(
-                      Icons.delete,
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
                       color: Colors.white,
+                      onPressed: this.onDelete,
                     ),
                     children: <Widget>[
                       Container(
@@ -162,13 +158,14 @@ class AddCategory extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   RaisedButton(
-                    textColor: Colors.blue,
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0)),
-                    child: Text('Add Subcategory'),
-                    onPressed: () {},
-                  ),
+                      textColor: Colors.blue,
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Text('Add Subcategory'),
+                      onPressed: () {
+                        show();
+                      }),
                   RaisedButton(
                     textColor: Colors.white,
                     color: Colors.blue,
@@ -188,3 +185,53 @@ class AddCategory extends StatelessWidget {
     );
   }
 }
+
+show() {
+  return SubCategory();
+}
+
+class SubCategory extends StatefulWidget {
+  @override
+  _SubCategoryState createState() => _SubCategoryState();
+}
+
+class _SubCategoryState extends State<SubCategory> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+            child: const Divider(
+              height: 20,
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue)),
+                labelText: 'Name',
+                hintText: 'Name'),
+          ),
+          const Divider(
+            height: 20,
+          ),
+          TextField(
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue)),
+                labelText: 'Description',
+                hintText: 'Description'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
